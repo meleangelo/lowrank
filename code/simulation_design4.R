c61a2851d73880b2eac530b6cb616980e9398ef3
meleangelo <angelo.mele@jhu.edu>
2022-08-25 15:11:39 -0400

    added simulation design 4 to readme and codes

---
+++ code/simulation_design4.R
@@ -0,0 +1,23 @@
+design <- 1
+# network sizes to simulate
+nvec <- c(2000,3000,4000,5000,6000,10000) 
+# number of simulations (not used yet)
+nsim <- 1000
+
+# other simulation parameters
+TT <- 5 # time periods
+K <- 2 # number of blocks
+d <- 1 # dimension of latent variables
+gamma <- 1.5  # persistence parameter
+
+dmax <- 5  # largest d to try for estimation
+
+
+# latent positions
+latent <- matrix(cbind(.8, -1.5), nrow = d, ncol = K)   
+
+# tables to store results
+table_max <- data.frame(matrix(NA, nrow = length(nvec), ncol = dmax+1))
+names(table_max) <- c("n", 1:dmax)
+table_mean <- data.frame(matrix(NA, nrow = length(nvec), ncol = dmax+1))
+names(table_mean) <- c("n", 1:dmax)
