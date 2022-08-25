6971ee43fcff3c2ce546f756b9839d4396fb27f3
Angelo Mele <amele1@jh.edu>
2022-08-25 16:56:36 -0400

    modified simulation.R

--- code/simulation.R
+++ code/simulation.R
@@ -44,3 +44,50 @@ source("code/setup_functions.R")
 source("code/simulation_design4.R")
 # run siulation
 source("code/simulate_loop.R")
+
+
+
+#### DESIGN 5
+rm(list=ls())
+# load functions needed for simulations
+source("code/setup_functions.R")
+# load simulation design
+source("code/simulation_design5.R")
+# run siulation
+source("code/simulate_loop.R")
+
+
+
+#### DESIGN 6
+rm(list=ls())
+# load functions needed for simulations
+source("code/setup_functions.R")
+# load simulation design
+source("code/simulation_design6.R")
+# run siulation
+source("code/simulate_loop.R")
+
+
+
+
+#### DESIGN 7
+rm(list=ls())
+# load functions needed for simulations
+source("code/setup_functions.R")
+# load simulation design
+source("code/simulation_design7.R")
+# run siulation
+source("code/simulate_loop.R")
+
+
+
+
+#### DESIGN 8
+rm(list=ls())
+# load functions needed for simulations
+source("code/setup_functions.R")
+# load simulation design
+source("code/simulation_design8.R")
+# run siulation
+source("code/simulate_loop.R")
+
